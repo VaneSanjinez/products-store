d3e37db05a21c1de2101f1c05c8b92b1f0e15a38
Vanessa Sanjinez <vanessa.sanjinez@truextend.com>
2019-02-19 16:42:27 -0400

    Delete unused code, modify var name, add condition with ternary operatos to change button styles according to a passed props

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,14 +5,18 @@ export const ButtonContainer = styled.button`
   font-size: 1.4rem;
   background: transparent;
   border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
+  border-color: ${props =>
+    props.cartButton ? "var(--mainYellow)" : "var(--lightBlue)"};
+  color: ${props =>
+    props.cartButton ? "var(--mainYellow)" : "var(--lightBlue)"};
   boder-radius: 0.5rem;
   padding: 0.2rem 0.5 rem;
   cursor: pointer;
   margin: 0.2rem 0.5rem 0.2rem 0;
   transition: all 0.5s ease-in-out;
   &:hover {
-    background: var(--lightBlue);
+    background: ${props =>
+      props.cartButton ? "var(--mainYellow)" : "var(--lightBlue)"};
     color: var(--mainBlue);
   }
   &:focus {
